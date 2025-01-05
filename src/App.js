02848919e2e46512d00159afe3535bb185778f04
Vishal Jangid <vishaljangid2@outlook.com>
2025-01-05 09:35:22 +0700

    Project Setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import {useRef} from 'react';
+import * as facemesh from '@tensorflow-models/facemesh';
+import Webcam from 'react-webcam';
+
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
